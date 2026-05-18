1bb7fedfc225ae26c1585e3ace69a84232d226bc
Stefano Pala <stfn.paletta@gmail.com>
2026-05-18 18:12:26 +0100

    Updating CMake to create tests and starting the test suite creation

---
+++ 1_NumToy/tests/test_ntoyarray.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Martina on 18/05/2026.
+//
+
+#include <gtest/gtest.h>
+#include "../include/ntoyarray.h"
+
+TEST(NToyArrayTest, Summation)
+{
+    numtoy::NToyArray multi_dim_array({2,2});
+}
